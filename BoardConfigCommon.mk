310873c582b4ab4e5d7e05ce0c11f1c90f115e8d
FlowerSea0208 <zzp060208@gmail.com>
2022-08-18 00:56:13 +0800

    [TMP]lahaiana: Ignore neverallows

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -190,6 +190,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
